274eac3c4579a6762b95fb1927a779a70b8df914
MiguelMerino <ing.miguela.merinomtz@gmail.com>
2024-10-18 14:30:13 -0600

    eliminar una carpeta

--- src/main/java/com/tecgurus/puntoventa/utils/Utilidades.java
+++
@@ -1,8 +0,0 @@
-package com.tecgurus.puntoventa.utils;
-
-import org.springframework.stereotype.Component;
-
-@Component //  el componente es parte de core de spring boot es algo similar bean Autowired
-public class Utilidades {
-	
-}
