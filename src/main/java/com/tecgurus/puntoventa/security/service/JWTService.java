86a45ddc020e47a3e14533af25260e4e2efc3a04
MiguelMerino <ing.miguela.merinomtz@gmail.com>
2024-10-18 14:02:35 -0600

    agregando anotacion value para obtener valores desde el archivo properties

--- src/main/java/com/tecgurus/puntoventa/security/service/JWTService.java
+++ src/main/java/com/tecgurus/puntoventa/security/service/JWTService.java
@@ -4,23 +4,24 @@ import java.time.Duration;
 import java.time.Instant;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Value;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 
 @Service
+
 public class JWTService {
 	
 	private static final Duration JWT_TOKEN_IAT = Duration.ofMinutes(60);
 	
 	private final Algorithm hmac512;
 	private final JWTVerifier verifier;
+
+	//private String secret;
 	
-	private String secret = "$2a$12$x5YdY3x12RJ41yWot9Lt4.LmDjmrAvOWgE3A7Hs5fvKkph2JAb166";
 	
-	
-	public JWTService() {
+	public JWTService(@Value("${spring.application.secret.key}") String secret) {
 		this.hmac512 = Algorithm.HMAC512(secret);
 		this.verifier = JWT.require(hmac512).build();
 	}
